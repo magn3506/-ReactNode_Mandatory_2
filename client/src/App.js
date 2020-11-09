2e6e1c19cef2415acdae35107e0903049422d4b0
MagnusVJensen <magnusvjensen@gmail.com>
2020-11-09 11:09:28 +0100

    added logout

--- client/src/App.js
+++ client/src/App.js
@@ -8,16 +8,17 @@ import { PublicRoute } from "./routes/public.route";
 // PAGES
 import APP from "./pages/app/App";
 import LOGIN from "./pages/login/Login";
-
+import SIGNUP from "./pages/signup/Signup";
 
 function App() {
 
   return (
     <Router>
       <Switch>
-        <ProtectedRoute exact path="/app" component={APP} />
         <PublicRoute path="/" component={LOGIN} />
         <PublicRoute path="/login" component={LOGIN} />
+        <PublicRoute path="/signup" component={SIGNUP} />
+        <ProtectedRoute exact path="/app" component={APP} />
       </Switch>
     </Router>
 
