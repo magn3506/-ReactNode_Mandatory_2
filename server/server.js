e033d686bee476afa6a3989b8c9e6ad576c0adbe
MagnusVJensen <magnusvjensen@gmail.com>
2020-11-13 16:54:19 +0100

    rdy to build

--- server/server.js
+++ server/server.js
@@ -20,8 +20,7 @@ app.use(express.json());
 // RATE LIMITER
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
-// app.set('trust proxy', 1);
-
+app.set('trust proxy', 1);
 const apiLimiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 minutes
     max: 100
